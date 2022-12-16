776cb36ff383facddc40a14c66229a94e060cac9
buraksahin <buraksahin.tr@gmail.com>
2022-12-16 22:34:16 +0300

    add files

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Link to='/'>Main</Link>
+      <Link to='/about'>About</Link>
+    </footer>
+  )
+}
+
+export default Footer
